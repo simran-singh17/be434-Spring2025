06ef484c364581708d0d1b483fe166eb60dabf70
Bonnie Hurwitz <bhurwitz@arizona.edu>
2025-05-03 00:12:12 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,8 +1,11 @@
 {pkgs}: {
   deps = [
     pkgs.vim
+<<<<<<< HEAD
     pkgs.python312Packages.conda
     pkgs.imagemagick_light
     pkgs.haskellPackages.PropLogic
+=======
+>>>>>>> 5389004 (Fixes By Bonnie)
   ];
 }
